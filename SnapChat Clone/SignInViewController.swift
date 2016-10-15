e8358f126090cb39ff5932f5162cb5a838972e18
ROHIT GUPTA <rohitgupta@ROHITs-MacBook-Pro-2.local>
2016-10-15 10:26:14 -0700

    first commit; added Firebase

---
+++ SnapChat Clone/SignInViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SignInViewController.swift
+//  SnapChat Clone
+//
+//  Created by ROHIT GUPTA on 10/9/16.
+//  Copyright © 2016 ROHIT GUPTA. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SignInViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+    @IBAction func showUpTapped(_ sender: AnyObject) {
+        
+        
+        
+    }
+
+}
+
